8ae0fd4d965a88ae0f6462460176853a5880e372
Nam Nguyễn Trọng <namnt.ndm@gmail.com>
2020-09-21 16:27:00 +0700

    add https

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App</code>
         </p>
         <a
           className="App-link"
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lear
         </a>
         <Alias />
       </header>
